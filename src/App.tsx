7eae2c5338fb4ee67786f0ea74a54b7c676a3d67
Nithindas004 <nithindas232001@gmail.com>
2024-06-02 17:37:48 +0530

    python code removed

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,10 @@ import Phc from "./admin/pages/Phc";
 import PhcForm from "./workers/PhcForm";
 import Createuser from "./custom-components/Createuser";
 import Form from "./admin/pages/loginForm";
-import Home from "./admin/pages/home";
+// import Home from "./admin/pages/home";
 import CreateCampaign from "./admin/pages/CreateCampaign";
 import CampaingTable from "./admin/pages/CampaingTable";
-import PreditcDiabetes from "./admin/pages/PreditcDiabetes";
+// import PreditcDiabetes from "./admin/pages/PreditcDiabetes";
 
 function App() {
   return (
@@ -21,7 +21,7 @@ function App() {
       <Switch>
         <Route exact path="/home" component={Phc}/>
         <Route path="/user" component={Createuser} />
-        <Route path="/predict-diabetes" component={PreditcDiabetes} />
+        {/* <Route path="/predict-diabetes" component={PreditcDiabetes} /> */}
         <Route path="/campaigns" component={CampaingTable} />
         <Route path="/create-campaign" component={CreateCampaign} />
         <Route path="/phc" component={Phc} />
